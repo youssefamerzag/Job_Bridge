7ad06ab1c39313b41be9d219d020180b8beee5cb
youssefamerzag <amerzag.youssef4@gmail.com>
2024-08-30 15:31:20 +0100

    add make search works in home page

--- index.php
+++ index.php
@@ -9,26 +9,43 @@
 </head>
 
 <?php
-    require "header.php";
-    require "db.php";
+require "header.php";
+require "db.php";
 
-    $select = $db->prepare('select * from offers');
-    $select->execute();
-    $offers = $select->fetchAll(PDO::FETCH_OBJ);
+$select = $db->prepare('select * from offers');
+$select->execute();
+$offers = $select->fetchAll(PDO::FETCH_OBJ);
+
+if (isset($_POST['search'])) {
+    $searchTerm = '%' . $_POST['search'] . '%';
+    $searchSql = $db->prepare("select * from offers where title like ?");
+    $searchSql->execute([$searchTerm]);
+    $offersBySearch = $searchSql->fetchAll(PDO::FETCH_OBJ);
+    $offers = $offersBySearch;
+}
+
+if (empty($offers)) {
+    $searchErr = 'The Jobs search returns no results.';
+}
 ?>
 
 <div class="indexBody">
     <header class="indexheader">
-        <div style="display: flex; gap: 5px; background-color: aliceblue; padding: 10px 10px 10px 10px; border-radius: 100px;">
-            <input class="indexSearchInput" type="text">
-            <button class="indexSearchButton">Search</button>
-        </div>
+        <form action="index.php" method="post" style="display: flex; gap: 5px; background-color: aliceblue; padding: 10px 10px 10px 10px; border-radius: 100px;">
+            <input name="search" class="indexSearchInput" type="text" value="<?php if (!empty($_POST['search'])) {
+                                                                                    echo $_POST['search'];
+                                                                                } else {
+                                                                                    '';
+                                                                                } ?>">
+            <button type="submit" class="indexSearchButton">Search</button>
+        </form>
         <p style="color: white;font-size: 25px;font-weight: 600;">For Job Seekers: Discover Your Next Opportunity</p>
     </header>
 
     <div style="display: flex; flex-direction:column;gap: 20px; margin: 30px 0px;justify-content: center;">
-        <div style="display: flex; justify-content: start; width: 50%; margin: 0px 30px">
+        <div style="display: flex;flex-direction: column;gap: 20px; justify-content: start; width: 50%; margin: 0px 30px">
             <p style="font-size: 35px;">Recommended Jobs</p>
+            <p><?php if(empty($offers)){ echo $searchErr; }?></p>
         </div>
         <div style="display: flex; justify-content: center; width: 100%;">
             <div class="offers">
